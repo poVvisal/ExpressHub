38292fd776a00dae70b360591df11ec0ba0a6d5a
kingczin <kingczinn@gmail.com>
2026-04-09 17:09:21 +0700

    add tf files

--- terraform/modules/ec2/user_data.sh
+++ terraform/modules/ec2/user_data.sh
@@ -9,9 +9,21 @@ sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 
 # Clone the repository
-git clone https://github.com/poVvisal/ExpressHub.git /home/ubuntu/ExpressHub
-cd /home/ubuntu/ExpressHub
+# If the directory exists, pull the latest changes
+if [ -d "/home/ubuntu/ExpressHub" ]; then
+  cd /home/ubuntu/ExpressHub
+  git pull
+else
+  git clone https://github.com/poVvisal/ExpressHub.git /home/ubuntu/ExpressHub
+  cd /home/ubuntu/ExpressHub
+fi
 
-# Build and run the Docker container
+# Build the Docker image
 sudo docker build -t foodexpress-js .
+
+# Stop and remove any existing container
+sudo docker stop foodexpress-js || true
+sudo docker rm foodexpress-js || true
+
+# Run the new container
 sudo docker run -d --name foodexpress-js -p 3000:3000 foodexpress-js
