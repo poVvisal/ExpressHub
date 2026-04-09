dc61b4c2053907b1014ef9c782bd95ba2b107681
kingczin <kingczinn@gmail.com>
2026-04-10 03:31:43 +0700

    add block size

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,7 +1,9 @@
 output "public_ip" {
-  value = aws_instance.foodexpress_server.public_ip
+  description = "Public IP addresses of the EC2 instances"
+  value       = aws_instance.foodexpress_server[*].public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.foodexpress_server.id
+  description = "IDs of the EC2 instances"
+  value       = aws_instance.foodexpress_server[*].id
 }
