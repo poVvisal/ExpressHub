0a779b7f1a380e41da3175e642cca2989df72579
kingczin <kingczinn@gmail.com>
2026-04-25 14:05:30 +0700

    improve the security to pass the quality gate

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "foodexpress_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["203.176.133.182/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
