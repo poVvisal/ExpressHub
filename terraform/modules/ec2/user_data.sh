cf2c7abddd1696f7023d6663cbf77712e061b010
Visal <visalgangster69@gmail.com>
2026-05-20 16:05:28 +0700

    update permissions

--- terraform/modules/ec2/user_data.sh
+++ terraform/modules/ec2/user_data.sh
@@ -10,6 +10,8 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo chmod 777 /var/run/docker.sock
+sudo chmod -R 777 /home/ubuntu
+sudo chmod 666 /var/run/docker.sock
 
 # 3. Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
