72bf5c4dbd1bf064cbe2e0119223bcf77c90a4c3
Visal <visalgangster69@gmail.com>
2026-05-20 10:20:22 +0700

    edit actions

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "http" {
+    address        = "https://tfstate.devExpressHub"
+    update_method  = "PUT"
+    lock_address   = "https://tfstate.devExpressHub/lock"
+    unlock_address = "https://tfstate.devExpressHub/unlock"
+    lock_method    = "LOCK"
+    unlock_method  = "LOCK"
+    username       = ""
+    password       = ""
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
